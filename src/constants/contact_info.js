99727ff8ef6187352b22c63685bddd96208b86c2
Svend Christian Svendsen <svendcs@svendcs.com>
2020-03-05 10:55:35 +0100

    Update contact info

--- src/constants/contact_info.js
+++ src/constants/contact_info.js
@@ -1,4 +1,4 @@
 export const CONTACT_PERSON = 'Svend Christian Svendsen';
-export const CONTACT_PHONE = 'phone number';
-export const CONTACT_EMAIL = 'email';
-export const CONTACT_OFFICE = 'office';
+export const CONTACT_PHONE = '20749109';
+export const CONTACT_EMAIL = 'svendcs@cs.au.dk';
+export const CONTACT_OFFICE = 'Nygaard 326';
